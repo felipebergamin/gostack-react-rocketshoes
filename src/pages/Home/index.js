4d584b10acec04e8322ce9399646a83583734c8f
Felipe Bergamin <felipebergamin6@gmail.com>
2019-09-16 21:49:35 -0300

    exibe quantidades no carrinho na Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -27,25 +27,27 @@ class Home extends Component {
   handleAddProduct = product => {
     const { addToCart } = this.props;
     addToCart(product);
-  }
+  };
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ProductList>
         {products.map(product => (
           <li key={product.id}>
-            <img
-              src={product.image}
-              alt={product.title}
-            />
+            <img src={product.image} alt={product.title} />
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button" onClick={() => this.handleAddProduct(product)}>
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
-                <MdAddShoppingCart size={16} color="#fff" /> 3
+                <MdAddShoppingCart size={16} color="#fff" />{' '}
+                {amount[product.id] || 0}
               </div>
 
               <span>Adicionar ao Carrinho</span>
@@ -57,8 +59,17 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
-
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
